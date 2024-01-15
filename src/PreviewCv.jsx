62954ae37f799e26a18efbc2dc08f5a24d106464
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2024-01-15 14:31:58 +1300

    Add placeholder information on the CV

--- src/PreviewCv.jsx
+++ src/PreviewCv.jsx
@@ -7,27 +7,28 @@ export default function PreviewCv(props) {
   return (
     <div className="cv-container">
       <header>
+        {!firstName && <p className="example">EXAMPLE</p>}
         <h1>
-          {firstName} {lastName}
+          {firstName ? firstName : "Jimmy"} {lastName ? lastName : "Conway"}
         </h1>
         <address>
-          <a href={email}>{email}</a>
-          <a href={phoneNum}>{phoneNum}</a>
-          <p>{address}</p>
+          <a href={email}>{email ? email : "name@email.com"}</a>
+          <a href={phoneNum}>{phoneNum ? phoneNum : "000-1234-5678"}</a>
+          <p>{address ? address : "New York City, New York"}</p>
         </address>
       </header>
       <main>
         <section>
           <h2>Education</h2>
           <div className="education-info-container">
-            <p>{startDate}</p>
-            <p>{endDate}</p>
-            <p>{location}</p>
+            <p>{startDate ? startDate : "1993-01-01"}</p>
+            <p>{endDate ? endDate : "2023-01-01"}</p>
+            <p>{location ? location : "New York City"}</p>
           </div>
           <div className="degree-container">
-            <p>{school}</p>
-            <p>{degree}</p>
-            <p>{major}</p>
+            <p>{school ? school : "Harvard University"}</p>
+            <p>{degree ? degree : "Bachelor of Arts"}</p>
+            <p>{major ? major : "History"}</p>
           </div>
         </section>
         <section>
@@ -36,14 +37,22 @@ export default function PreviewCv(props) {
               <div key={job.id}>
                 <h3>Work Experience</h3>
                 <div className="work-info-container">
-                  <p>{job.start}</p>
-                  <p>{job.end}</p>
-                  <p>{job.location}</p>
+                  <p>{job.start ? job.start : "2003-01-01"}</p>
+                  <p>{job.end ? job.end : "2023-01-01"}</p>
+                  <p>{job.location ? job.location : "New York City"}</p>
                 </div>
                 <div className="work-details-container">
-                  <p>{job.companyName}</p>
-                  <p>{job.title}</p>
-                  <p>{job.description}</p>
+                  <p>
+                    {job.companyName
+                      ? job.companyName
+                      : "Lucchese crime family"}
+                  </p>
+                  <p>{job.title ? job.title : "Family Associate"}</p>
+                  <p>
+                    {job.description
+                      ? job.description
+                      : "Organizing the Lufthansa heist."}
+                  </p>
                 </div>
               </div>
             );
