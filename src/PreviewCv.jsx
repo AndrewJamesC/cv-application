36df1357562b9733c887ab2b220abad946b67d62
AndrewJamesC <130733134+AndrewJamesC@users.noreply.github.com>
2024-01-15 14:15:22 +1300

    Destructure props in PreviewCv

--- src/PreviewCv.jsx
+++ src/PreviewCv.jsx
@@ -1,33 +1,33 @@
 /* eslint-disable react/prop-types */
 export default function PreviewCv(props) {
+  const { firstName, lastName, email, phoneNum, address } =
+    props.personalDetails;
+  const { startDate, endDate, location, school, degree, major } =
+    props.education;
   return (
     <div className="cv-container">
       <header>
         <h1>
-          {props.personalDetails.firstName} {props.personalDetails.lastName}
+          {firstName} {lastName}
         </h1>
         <address>
-          <a href={props.personalDetails.email}>
-            {props.personalDetails.email}
-          </a>
-          <a href={props.personalDetails.phoneNum}>
-            {props.personalDetails.phoneNum}
-          </a>
-          <p>{props.personalDetails.address}</p>
+          <a href={email}>{email}</a>
+          <a href={phoneNum}>{phoneNum}</a>
+          <p>{address}</p>
         </address>
       </header>
       <main>
         <section>
           <h2>Education</h2>
           <div className="education-info-container">
-            <p>{props.education.startDate}</p>
-            <p>{props.education.endDate}</p>
-            <p>{props.education.location}</p>
+            <p>{startDate}</p>
+            <p>{endDate}</p>
+            <p>{location}</p>
           </div>
           <div className="degree-container">
-            <p>{props.education.school}</p>
-            <p>{props.education.degree}</p>
-            <p>{props.education.major}</p>
+            <p>{school}</p>
+            <p>{degree}</p>
+            <p>{major}</p>
           </div>
         </section>
         <section>
